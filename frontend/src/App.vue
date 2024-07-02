67162d144227048edf1fc04f32a8961f11270633
MaduhSamp <madusampietro@gmail.com>
2024-07-02 17:39:04 -0300

    Formulário de registro implementado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+<div>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -17,41 +16,8 @@ import { RouterLink, RouterView } from 'vue-router'
   <main>
     <RouterView />
   </main>
+</div>
 </template>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-
-header {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: 20px;
-}
-
-.wrapper {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-
-nav {
-  display: flex;
-  gap: 20px;
-}
-
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-  width: 100%;
-}
-
 </style>
