0341faacc12cf7f123589e23d5b1e880cc33d4bb
Gustavo Hlusiak <gustavohlusiak@gmail.com>
2024-07-03 13:51:34 -0300

    conflitos branches

--- frontend/src/components/LoginRegisterComponent.vue
+++ frontend/src/components/LoginRegisterComponent.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="background">
     <div class="container">
         <div class="content">
             <h2 class="logo"> 3C+ </h2>
@@ -18,7 +19,7 @@
 
 
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <div class ="login-box">
+        <div class ="logreg-box">
             <div class="form-box login">
                 <form action="#">
                     <h2> Entrar na Conta </h2>
@@ -43,8 +44,45 @@
                     <div class="login-register">
                         <p> Nao tem uma Conta? <a href="#" class="register-link"> Registre-se</a></p>
                     </div>
+                    
                 </form>
             </div>
+
+
+            <div class="logreg-box">
+                <div class="form-box register">
+                    <form action="#">
+                        <h2>Sign Up</h2>
+
+                        <div class="input-box">
+                            <span class="icon"><i class='bx bxs-user'></i></span>
+                            <input type="text" required>
+                            <label>Name</label>
+                        </div>
+
+                        <div class="input-box">
+                            <span class="icon"><i class='bx bxs-envelope'></i></span>
+                            <input type="email" required>
+                            <label>Email</label>
+                        </div>
+                        <div class="input-box">
+                            <span class="icon"><i class='bx bxs-lock-alt'></i></span>
+                            <input type="password" required>
+                            <label>Password</label>
+                        </div>
+                        <div class="remember-forgot">
+                            <label><input type="checkbox">I agree to the terms and conditions</label>
+                        </div>
+                        
+                        <button type="submit" class="btn">Sign Up</button>
+
+                        <div class="login-register">
+                            <p>Already have an account? <a href="#" class="login-link">Sign in</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    </div>
 
 
         </div>
@@ -57,6 +95,12 @@ export default {
 }
 </script>
 <style scoped>
+
+.background {
+    width: 100%;
+    height: 100vh;
+    background: #000;
+}
 
     .container {
         position: absolute;
@@ -94,7 +138,7 @@ export default {
         font-size: 32px;
     }
 
-    .container .login-box{
+    .container .logreg-box{
         position: absolute;
         top: 0;
         right: 0;
@@ -104,11 +148,20 @@ export default {
     }
 
     .btn{
-        background: #3057F2;
-        color: white;
+        width: 100%;
+        height: 45px;
+        background: #c4103d;
+        border: none;
+        outline: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 16px;
+        color: #e4e4e4;
+        font-weight: 500;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .5);
     }
 
-    .login-box .form-box{
+    .logreg-box .form-box{
         display: flex;
         justify-content: center;
         align-items: center;
@@ -158,14 +211,45 @@ export default {
 
     .input-box input:focus~label,
     .input-box input:valid~label{
-        top: -5%;
+        top: -5px;
     }
 
     .input-box .icon{
         position: absolute;
         top: 13px;
         right: 0;
-        font-size: 20px;
+        font-size: 19px;
     }
+
+    .form-register-box .remember-forgot {
+    font-size: 14.5px;
+    font-weight: 500;
+    margin: -15px 0 15px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.remember-forgot label input {
+    accent-color: #e4e4e4;
+    margin-right: 3px;
+}
+
+
+.form-register-box .login-register {
+    font-size: 14.5px;
+    font-weight: 500;
+    text-align: center;
+    margin-top: 25px;
+}
+
+.login-register p a {
+    color: #e4e4e4;
+    font-weight: 600;
+    text-decoration: none;
+}
+
+.login-register p a:hover {
+    text-decoration: underline;
+}
 
 </style>
